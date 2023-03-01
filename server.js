378afc6aeab7c11121214140b57c5d95fde4d784
Adwait Arajaria <adeveloper121@gmail.com>
2023-03-01 20:30:10 +0530

    Initial commit

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
 const WebSocket = require('ws');
-const server = new WebSocket.Server({ port: '9386' });
+const server = new WebSocket.Server({ port: '8080' });
 
 server.on('connection', socket => {
-    console.log("Client Connected");
     socket.on('message', message => {
-        console.log("Client sent message: " + message);
         server.clients.forEach(client => {
             if(client != socket && client.readyState === WebSocket.OPEN){
                 client.send(message);
